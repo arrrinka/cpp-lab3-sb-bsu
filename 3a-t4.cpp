35ff574f2715650cc013dea5f080544f2f4e69f8
arrrinka <annirras@gmail.com>
2024-10-28 13:47:17 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,18 +1,37 @@
 #include <iostream> 
+#include <limits> 
 using namespace std;
-
 int main() {
     int k, i = 1, sum_k = 0, number;
     cout << "Enter the amount of integers to sum = ";
-    cin >> k;
-    // Check if k is zero
-    if (k == 0) {
+    while (true) {
+        cin >> k;
+        if (cin.fail() || k < 0) {         // Check if k is negative
+            cout << "Invalid input. Please enter a non-negative integer: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else {
+            break;
+        }
+    }
+    if (k == 0) {     // Check if k is zero
         cout << "The total sum of " << k << " integers is: 0" << endl;
-        return 0; // Exit the program since there are no integers to sum
+        return 0;
     }
     do {
         cout << "Enter integer nr. " << i << ": ";
-        cin >> number;
+        while (true) {
+            cin >> number;
+            if (cin.fail()) {
+                cout << "Invalid input. Please enter an integer: ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            else {
+                break;
+            }
+        }
         sum_k += number;
         i++;
     } while (i <= k);
